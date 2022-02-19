60abdfb3b4a9025ad9fd947b1c7b04411a6d6247
Franjo Ivankovic <29928096+FranjoIM@users.noreply.github.com>
2022-02-19 17:15:35 -0500

    Update 01_MarkerMatch.sh

--- 00_Resources/SLURM_Scripts/01_MarkerMatch.sh
+++ 00_Resources/SLURM_Scripts/01_MarkerMatch.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=MarkerMatch              # Job name	
-#SBATCH --mail-type=END,FAIL                # Mail events
-#SBATCH --mail-user=njofrica@ufl.edu        # Where to send mail	
-#SBATCH --ntasks=1                          # Number of tasks
-#SBATCH --mem=15gb                          # Per processor memory
-#SBATCH --time=05:00:00                     # Walltime
+#SBATCH --job-name=MarkerMatch
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=njofrica@ufl.edu
+#SBATCH --ntasks=1
+#SBATCH --mem=15gb
+#SBATCH --time=10:00:00
+#SBATCH --output=/00_Resources/SLURM_Logs/01-MarkerMatch.log
 
 date; hostname; pwd
 WorkDir=$(pwd)
@@ -16,8 +17,8 @@ mkdir -p ${WorkDir}/01_MarkerMatch/Outputs
 module load R/4.1
 
 # Run R Marker Match script ------------------------------------------- 
-Rscript ${WorkDir}/01_MarkerMatch/MarkerMatch.R
-  --ref
-  --mat
-  --dist
-  --out
+Rscript ${WorkDir}/01_MarkerMatch/MarkerMatch.R \
+  --ref GSA-24v1-0_C1.csv \
+  --mat ${WorkDir}/00_Resources/Manifests \
+  --dist 10000 \
+  --out ${WorkDir}/01_MarkerMatch/Outputs
