a53271db1d211d370baf62aeb1f1cf29796b74e1
Franjo Ivankovic <29928096+FranjoIM@users.noreply.github.com>
2022-03-16 18:01:50 -0400

    Update PennCNV_TS_1.4.sh

--- 02_InitialCalling/CallCNVs/PennCNV_TS_1.4.sh
+++ 02_InitialCalling/CallCNVs/PennCNV_TS_1.4.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1                                                                # Run a single task
 #SBATCH --cpus-per-task=1                                                         # Number of CPU cores per task
 #SBATCH --mem=8gb                                                                 # Total memory limit
-#SBATCH --time=48:00:00                                                           # Time limit hrs:min:sec
+#SBATCH --time=96:00:00                                                           # Time limit hrs:min:sec
 #SBATCH --output=/blue/carolmathews/njofrica/CNV_TRIO/LogFiles/%x-%j.log          # Standard output and error log
 
 #-----------------------------------------------------------------------------------------------------------------
@@ -18,74 +18,83 @@ date;hostname;pwd
 # Move to CNV TRIO branch
 cd /blue/carolmathews/njofrica/CNV_TRIO
 
-# Declare CNV TRIO branch a working directory
-WKDIR="/blue/carolmathews/njofrica/CNV_TRIO"
-
 # Specify the batch
 BATCH="TS_1.4"
 
-# Define the batch stem
-FR="${WKDIR}/02_InitialCalling/Files/${BATCH}"
-
 # Create necessary directories
-mkdir -p ${FR}/run_PennCNV/data_aux
-mkdir -p ${FR}/run_PennCNV/results/adjusted
-mkdir -p ${FR}/run_PennCNV/results/raw
-mkdir -p ${FR}/run_PennCNV/results/logs
+mkdir -p /blue/carolmathews/njofrica/CNV_TRIO/02_InitialCalling/Files/${BATCH}/run_PennCNV/results/adjusted
+mkdir -p /blue/carolmathews/njofrica/CNV_TRIO/02_InitialCalling/Files/${BATCH}/run_PennCNV/results/raw
+mkdir -p /blue/carolmathews/njofrica/CNV_TRIO/02_InitialCalling/Files/${BATCH}/run_PennCNV/results/logs
 
-# Load necessary modules
-module load perl/5.24.1
-module load penncnv/1.0.5
-
-# Export necessary paths
-export PENNCNV=${HPC_PENNCNV_DIR}
+# Create variables and directory paths
+PENNCNV_DIR=/apps/penncnv/1.0.5
+workdir="/blue/carolmathews/njofrica/CNV_TRIO/02_InitialCalling/Files/${BATCH}/run_PennCNV"
+frPath="/blue/carolmathews/njofrica/CNV_TRIO/02_InitialCalling/Files/${BATCH}/run_PennCNV/data"
+outputdir="/blue/carolmathews/njofrica/CNV_TRIO/02_InitialCalling/Files/${BATCH}/run_PennCNV/results"
+SNPpos="/blue/carolmathews/njofrica/CNV_TRIO/02_InitialCalling/Files/${BATCH}/run_PennCNV/SNP_pos_aut.txt"
 
 #-----------------------------------------------------------------------------------------------------------------
 # PENN CNV -------------------------------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------------------------------
 
-# Create a list of files for processing
-ls ${FR}/run_PennCNV/data/* > ${FR}/run_PennCNV/data_aux/list_pfb.txt
+# Create a list of SNPs
+awk '{ if((NR == 1) || ($2 >= 1 && $2 <= 22)) { print } }' \
+  /blue/carolmathews/njofrica/CNV_TRIO/02_InitialCalling/Files/${BATCH}/IntensityMatrices/SNP_pos.txt > \
+  ${SNPpos}
 
-# Remove non-autosomes from analysis
-awk '$2>0 && $2<23 {print}' ${FR}/IntensityMatrices/SNP_pos.txt > ${FR}/IntensityMatrices/SNP_pos_aut.txt
+# Create a list of samples from frPath
+ls ${frPath} > ${workdir}/samples
+samples="${workdir}/samples"
 
-# Decompress the input GC file
-zcat ${PENNCNV_DIR}/gc_file/hg19.gc5Base.txt.gz | \
-  sort -k2,2 -k3,3n > \
-  ${FR}/run_PennCNV/data_aux/hg19.gc5Base_sorted.txt
-  
-# Make GC model
-perl ${PENNCNV}/cal_gc_snp.pl \
-  --output ${FR}/run_PennCNV/data_aux/input.hg19.gcmodel \
-  ${FR}/run_PennCNV/data_aux/hg19.gc5Base_sorted.tx \
-  ${FR}/IntensityMatrices/SNP_pos_aut.txt
+# Load PennCNV Module
+module load penncnv/1.0.5
 
-# Compile PFB
-perl ${PENNCNV}/compile_pfb.pl \
-  -snpposfile ${FR}/IntensityMatrices/SNP_pos_aut.txt \
-  -listfile ${FR}/run_PennCNV/data_aux/list_pfb.txt \
-  -output ${FR}/run_PennCNV/data_aux/SNP.pfb
+# define sample information for running
+sample=$1
+N=$(ls ${samples} | wc -l)
 
-# GC adjust files and call CNVs
-cat ${FR}/run_PennCNV/data_aux/list_pfb.txt | while read item || [[ -n $line ]]
+echo "1. Decompress the input gc file"
+time zcat ${PENNCNV_DIR}/gc_file/hg19.gc5Base.txt.gz |\
+  sort -k2,2 -k3,3n \
+  > ${workdir}/hg19.gc5Base_sorted.txt
+ 
+echo "2. Make a gcmodel file for dataset..."
+time (
+  ${PENNCNV_DIR}/cal_gc_snp.pl \
+    --output ${workdir}/input.hg19.gcmodel \
+    ${workdir}/hg19.gc5Base_sorted.txt \
+    ${SNPpos}
+  )
+ 
+echo "3. Make a pfb file for dataset..."
+time (
+  ls ${frPath}/* > ${workdir}/intensity_data.adjusted.list
+  ${PENNCNV_DIR}/compile_pfb.pl \
+    --output ${workdir}/dataset.pfb \
+    --snpposfile ${SNPpos} \
+    --listfile ${workdir}/intensity_data.adjusted.list
+  )
+
+cat ${samples} | while read item || [[ -n $line ]]
 do
+  echo "4. Adjust for genomic wave..."
   time (
-    ${HPC_PENNCNV_DIR}/genomic_wave.pl \
-    --prefix ${FR}/run_PennCNV/results/adjusted \
+    ${PENNCNV_DIR}/genomic_wave.pl \
+    --prefix ${outputdir}/adjusted/ \
     -adjust \
-    -gcmodel ${FR}/run_PennCNV/data_aux/input.hg19.gcmodel \
-    ${FR}/run_PennCNV/data/${item}
+    -gcmodel ${workdir}/input.hg19.gcmodel \
+    ${frPath}/${item}
     )
     
+  echo "5. call cnv using penncnv..."
   time (
-    ${HPC_PENNCNV_DIR}/detect_cnv.pl \
+    ${PENNCNV_DIR}/detect_cnv.pl \
     -test \
-    -hmm ${HPC_PENNCNV_DIR}/lib/hhall.hmm \
-    -pfb ${FR}/run_PennCNV/data_aux/SNP.pfb \
-    --gcmodelfile ${FR}/run_PennCNV/data_aux/input.hg19.gcmodel \
-    -log ${FR}/run_PennCNV/results/logs/${item}.log \
-    -out ${FR}/run_PennCNV/results/raw/${item}.raw \
-    ${FR}/run_PennCNV/results/adjusted/${item}.adjusted
+    -hmm ${PENNCNV_DIR}/lib/hhall.hmm \
+    -pfb ${workdir}/dataset.pfb \
+    --gcmodelfile ${workdir}/input.hg19.gcmodel \
+    -log ${outputdir}/logs/${item}.log \
+    -out ${outputdir}/raw/${item}.raw \
+    ${outputdir}/adjusted/${item}.adjusted
     )
 done;
